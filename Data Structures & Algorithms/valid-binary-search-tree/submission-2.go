8acfecadfa330382762bd6227411b9342a4f2c31
looser97 <45934605+looser97@users.noreply.github.com>
2026-07-23 14:18:54 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+    if root == nil {
+		return true
+	}
+
+	var dfs func(node *TreeNode, min int, max int) bool
+	dfs = func(node *TreeNode, min int, max int) bool {
+		if node == nil {
+			return true
+		}
+
+		if node.Val <= min || node.Val >= max {
+			return false
+		}
+		return dfs(node.Left, min, node.Val) && dfs(node.Right, node.Val, max)
+	}
+	return dfs(root, math.MinInt64, math.MaxInt64)
+}
