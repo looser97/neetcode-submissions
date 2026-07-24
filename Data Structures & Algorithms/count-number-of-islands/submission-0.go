3c75610840731884f5b533448897b17ec43d56f2
looser97 <45934605+looser97@users.noreply.github.com>
2026-07-24 14:08:51 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.go
@@ -0,0 +1,35 @@
+func numIslands(grid [][]byte) int {
+	visited := make([][]bool, len(grid))
+	for i := range visited {
+		visited[i] = make([]bool, len(grid[0]))
+	}
+	instlands := 0
+
+	var dfs func(i, j int)
+
+	dfs = func(i, j int) {
+		visited[i][j] = true
+		if i > 0 && grid[i-1][j] == '1' && !visited[i-1][j] {
+			dfs(i-1, j)
+		}
+		if i+1 < len(grid) && grid[i+1][j] == '1' && !visited[i+1][j] {
+			dfs(i+1, j)
+		}
+		if j > 0 && grid[i][j-1] == '1' && !visited[i][j-1] {
+			dfs(i, j-1)
+		}
+		if j+1 < len(grid[0]) && grid[i][j+1] == '1' && !visited[i][j+1] {
+			dfs(i, j+1)
+		}
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if !visited[i][j] && grid[i][j] == '1' {
+				instlands++
+				dfs(i, j)
+			}
+		}
+	}
+
+	return instlands
+}
